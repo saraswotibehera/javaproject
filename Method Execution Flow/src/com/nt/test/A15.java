801db527ca7631c481c101060245b55aab63bd8e
saraswotibehera <49040479+saraswotibehera@users.noreply.github.com>
2019-03-29 00:03:27 +0530

    Add files via upload

---
+++ Method Execution Flow/src/com/nt/test/A15.java
@@ -0,0 +1,16 @@
+package com.nt.test;
+
+public class A15 {
+
+	
+	private void m1() {
+		System.out.println("a m1");
+	}
+	void m2() {
+		System.out.println("a m2");
+		m1();
+	}
+
+	}
+
+
---
+++ Method Execution Flow/src/com/nt/test/B15.java
@@ -0,0 +1,18 @@
+package com.nt.test;
+
+ class B15 extends  A15{
+	 
+	 void m1() {
+		System.out.println("b m1");
+			}
+public static void main(String[] args) {
+	B15 b=new B15();
+	b.m1();
+	b.m2();
+	A15 a=new B15();
+	//a.m1();
+	a.m2();
+	
+
+			}
+}
